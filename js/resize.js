3d3411b30cfb0003a72eb43bf558c09c6d521190
0tho <marcelo.lopes.lotufo@gmail.com>
2016-09-03 03:23:20 -0300

    Update portfolio page

--- js/resize.js
+++ js/resize.js
@@ -1,23 +1,14 @@
 (function () {
-  window.onload(function () {
+  function setFontSize () {
     var html = document.querySelector('html');
-    var logo_container = document.getElementsByClassName('logo_container')[0];
-    var width = logo_container.clientWidth;
+    var width = html.clientWidth;
     var fontSize = width / 150;
 
     fontSize = +fontSize.toFixed(2);
 
     html.setAttribute('style', 'font-size: '+ fontSize +'px')
+  }
 
-    window.onresize(function() {
-      var html = document.querySelector('html');
-      var logo_container = document.getElementsByClassName('logo_container')[0];
-      var width = logo_container.clientWidth;
-      var fontSize = width / 150;
-
-      fontSize = +fontSize.toFixed(2);
-
-      html.setAttribute('style', 'font-size: '+ fontSize +'px')
-    });
-  });
+  window.onload(setFontSize);
+  window.onresize(setFontSize);
 });
